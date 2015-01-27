1e22086fa8cdd88e4186b98f544698e8c05739ec
HermannBjorgvin <hermann3646@gmail.com>
2015-01-27 18:05:35 +0000

    Sails.js Framework along with a 'completed' db
    At this point a robot should be able to subscribe to commands and output
    statuses and logs. As well as send commands with parameters to the
    robot. What is left is to add a nifty dashboard that allows full
    customization of all messages and commands that are sent.

---
+++ api/controllers/CommandsController.js
@@ -0,0 +1,11 @@
+/**
+ * CommandsController
+ *
+ * @description :: Server-side logic for managing commands
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/StatusesController.js
@@ -0,0 +1,11 @@
+/**
+ * StatusesController
+ *
+ * @description :: Server-side logic for managing statuses
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
